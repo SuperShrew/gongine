b256529f4a8bd2842ab6b4720ee8195b28ba8fc2
Clover Gerrish <150274640+SuperShrew@users.noreply.github.com>
2025-12-09 13:25:25 +0000

    commenting

--- engine.go
+++ engine.go
@@ -6,9 +6,9 @@ type sprite struct {
 	x,y int
 	coords [][]int
 	structure [][]tile
-	i1 func()
-	i2 func()
-	i3 func()
+	//i1 func()
+	//i2 func()
+	//i3 func()
 	update func()
 	anims []string
 }
@@ -25,39 +25,39 @@ type tile struct {
 //}
 
 func createEnvironment(x, y int, background string) [][]tile {
-	env := make([][]tile, y)
+	env := make([][]tile, y) // allocates zeroed array and returns a slice referring to the array (for more malleable arrays)
 	for i:=0; i<y; i++ {
 		env[i] = make([]tile, x)
 		for j:=0; j<x; j++ {
-			env[i][j] = tile{background, false, nil}
+			env[i][j] = tile{background, false, nil} // enabled by make()
 		}
 	}
 	return env
 }
 
-func renderEnvironment(env [][]tile) {
+func renderEnvironment(env [][]tile) { //simple iterate through 2D list and output func
 	for _, row := range env {
 		for _, t := range row {
-			fmt.Print(t.icon)
+			fmt.Print(t.icon) 
 		}
 		fmt.Println()
 	}
 }
 
-func placeSprite(s *sprite, env [][]tile) ([][]tile, error){
-	if s.x < 0 || s.y < 0 || s.y >= len(env) || s.x >= len(env[0]) {
-		return nil, fmt.Errorf("Sprite position out of bounds")
+func placeSprite(s *sprite, env [][]tile) ([][]tile, error){ // ooooh this is gonna take some explaining
+	if s.x < 0 || s.y < 0 || s.y >= len(env) || s.x >= len(env[0]) { // checks if the sprite's top left coordinates are out of bounds
+		return nil, fmt.Errorf("sprite position out of bounds") // return error and nil
 	}
-	for _, coord := range s.coords {
-		tileX := s.x + coord[0]
+	for _, coord := range s.coords { // iterates through stored sprite tile positions
+		tileX := s.x + coord[0] // assigning x y coords
 		tileY := s.y + coord[1]
-		if tileX < 0 || tileY < 0 || tileY >= len(env) || tileX >= len(env[0]) {
-			return nil, fmt.Errorf("Sprite coordinate %v , %v out of bounds", coord[1], coord[0])
+		if tileX < 0 || tileY < 0 || tileY >= len(env) || tileX >= len(env[0]) { // if any of the coords contained in the sprite's hitbox are out of bounds
+			return nil, fmt.Errorf("sprite coordinate %v , %v out of bounds", coord[1], coord[0])
 		}
-		if env[tileY][tileX].solid {
-			return nil, fmt.Errorf("Collision detected at %v , %v", tileX, tileY)
+		if env[tileY][tileX].solid { // also if any tiles of the sprite intersect a solid tile
+			return nil, fmt.Errorf("collision detected at %v , %v", tileX, tileY)
 		}
-		env[tileY][tileX] = s.structure[coord[1]][coord[0]]
+		env[tileY][tileX] = s.structure[coord[1]][coord[0]] // after all checks passed then tile is written to LOCAL FUNCTION environment variable and NOT the public one (since checks can still fail)
 	}
 	return env, nil
 }
